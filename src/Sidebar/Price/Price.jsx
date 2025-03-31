81202f754a7e80b9293470bc88b8689e943b391f
jay9758 <163132972+jay9758@users.noreply.github.com>
2025-03-31 13:51:12 +0530

    Update Price.jsx

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Price.css'
-import Input from '../../components/input'
+import Input from '../../components/Input'
 
 const Price = ({handleChange}) => {
   return (
